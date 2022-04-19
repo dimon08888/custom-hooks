b32fd2718f2962f69642f8822afc17d2bfa34f74
dimon08888 <dimon08888@mail.ru>
2022-04-19 19:40:38 +0300

    Добавлен useSessionStorage hook

--- src/hooks/useSessionStorage.ts
+++ src/hooks/useSessionStorage.ts
@@ -1,3 +1,28 @@
-// TODO HOMEWORK
+import { useState, useEffect, useCallback } from 'react'
 
-export {}
+export default function useSessionStorage<T>(key: string, initialValue: T | (() => T)) {
+  const [value, setValue] = useState(initialValue)
+
+  useEffect(() => {
+    try {
+      const value = window.sessionStorage.getItem(key)
+      if (value !== null) {
+        setValue(JSON.parse(value))
+      }
+    } catch {}
+  }, [key])
+
+  useEffect(() => {
+    try {
+      window.sessionStorage.setItem(key, JSON.stringify(value))
+    } catch {}
+  }, [key, value])
+
+  const removeCounter = useCallback(() => {
+    try {
+      window.sessionStorage.removeItem(key)
+    } catch {}
+  }, [key])
+
+  return [value, setValue, removeCounter]
+}

01a7cfad2aff33914a8e695d987d8b464d5e709c
dimon08888 <dimon08888@mail.ru>
2022-05-13 13:54:08 +0300

    Добавлено ToastExample

--- src/App.tsx
+++ src/App.tsx
@@ -1,208 +1,76 @@
 import React from 'react'
 import './App.css'
-import UseContextExample from './components/useContextExample'
-import UseFetchExample from './components/UseFetchExample'
-import { Chat, StopWatch, Counter } from './components/UseRefExample'
-import { UseEffectExample } from './components/useUseEffectExample'
-import { ToastProps } from './toast/Toast'
-import Unmount from './utils/Unmount'
-import { useState } from 'react'
-import { User } from './context/class'
-import { TitleContextExample } from './context/function'
-import RenderPropsExample from './context/render-props'
+import { ToastProvider, useToast } from './toast/Toast'
 
-function ToastExample({ toast }: { toast: (props: ToastProps) => void }) {
+function App() {
   return (
-    <div>
-      <button
-        onClick={() =>
-          toast({ type: 'success', message: 'Hello World', position: 'bottom-right' })
-        }
-      >
-        Make me a toast
-      </button>
+    <div className='App'>
+      <ToastProvider>
+        <RegisterForm />
+        <Menu />
+        <DeletePost />
+      </ToastProvider>
     </div>
   )
 }
 
-function SendMessageForm({ sendMessage }: { sendMessage: (message: string) => void }) {
-  const inputRef = React.useRef<HTMLInputElement>(null)
+function RegisterForm() {
+  const toast = useToast()
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    toast({ color: 'green', message: 'Successful registration' })
+  }
+
   return (
-    <div>
-      <input type='text' ref={inputRef} />
-      <button onClick={() => sendMessage(inputRef.current!.value)}>Send message</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type='text' placeholder='Enter name' />
+      <br />
+      <button type='submit'>Register</button>
+    </form>
   )
 }
 
-function MessageList({ messages }: { messages: string[] }) {
+function Menu() {
+  const toast = useToast()
+
+  function handleLogoutClick() {
+    // logout
+    toast({ message: 'Logged out', color: 'blue' })
+  }
+
   return (
     <ul>
-      {messages.map(str => (
-        <li>{str}</li>
-      ))}
+      <li>Home</li>
+      <li>Feed</li>
+      <li>Profile</li>
+      <li>
+        <button onClick={handleLogoutClick}> Logout</button>
+      </li>
     </ul>
   )
 }
 
-function App() {
-  const [messages, setMessages] = useState<string[]>([])
-  function sendMessage(str: string) {
-    setMessages(prevMessages => {
-      const newMessages = prevMessages.slice()
-      newMessages.push(str)
-      return newMessages
-    })
-  }
+function DeletePost() {
+  const toast = useToast()
 
-  const [toasts, setToasts] = useState<ToastProps[]>([])
-  function sendToast(props: ToastProps) {
-    setToasts(prevToasts => {
-      const newToasts = prevToasts.slice()
-      newToasts.push(props)
-      return newToasts
-    })
+  function handlePostDelete() {
+    // delete
+    toast({ message: 'Error: can not delete post', color: 'red' })
   }
 
   return (
-    <div className='App'>
-      {/* <ToggleExample /> */}
-      {/* <ArrayExample /> */}
-      {/* <CounterExample /> */}
-      {/* <StateWithValidationExample /> */}
-      {/* <Unmount>
-        <StopWatch />
-      </Unmount> */}
-      {/* <Chat /> */}
-      {/* <Counter /> */}
-      {/* <UseEffectExample /> */}
-      {/* <UseFetchExample /> */}
-      {/* <ClickOutsideExample /> */}
-      {/* <ClassComponent /> */}
-      {/* <ToastContext.Provider value={toast }> */}
-      {/* <TitleContextExample /> */}
-      {/* <SendMessageForm sendMessage={sendMessage} /> */}
-      {/* <MessageList messages={messages} /> */}
-      <RenderPropsExample />
-      {/* <User /> */}
-      {/* <MessageExample sendMessage={undefined as any} />
-      {<div></div> } */}
-
-      {/* <ToastExample toast={sendToast} /> */}
-
-      {/* </ToastContext.Provider> */}
-      {/* <UseContextExample /> */}
-      {/* {isMounted && <RefExample />}
-      <button onClick={() => setIsMounted(false)}>Unmount</button> */}
-      {/* {isMounted && <IntervalExample />}
-      <button onClick={() => setIsMounted(false)}>Unmount</button>
-
-      {/* <StateWithHistoryExample /> */}
-      {/* <RenderCountExample /> */}
-      {/* <MergedStateExample /> */}
-      {/* <PreviousExample /> */}
-      {/* <UpdateEffectExample /> */}
-      {/* <LocalStorageExample /> */}
+    <div>
+      <textarea defaultValue='This is a post text'></textarea>
+      <button onClick={handlePostDelete}>Delete post</button>
     </div>
   )
 }
 
-// class ClassComponent extends React.Component {
-//   element: HTMLDivElement | null = null
-
-//   componentDidMount() {
-//     console.log(this.element)
-//   }
-
-//   componentDidUpdate() {
-//     this.element = new HTMLDivElement()
-//   }
-
-//   render() {
-//     return <div ref={element => (this.element = element)}>Hello</div>
-//   }
-// }
-
-// function Counter() {
-//   const [obj, setObj] = useState({ current: 0 })
-//   const { increment } = useCounter(0)
-
-//   return (
-//     <div>
-//       <div>Counter: {obj.current}</div>
-//       <button onClick={() => increment()}></button>
-//     </div>
-//   )
-// }
-
-// function InputExample() {
-//   // const ref = useRef<HTMLInputElement>(null)
-//   const ref = useRef<HTMLSelectElement>(null)
-
-//   function handleInputColor() {
-//     if (ref.current) {
-//     }
-//   }
-//   return (
-//     <div>
-//       <select ref={ref}>
-//         <option>hop</option>
-//         <option>hep</option>
-//         <option>hap</option>
-//         <option>hyp</option>
-//       </select>
-//       <button onClick={handleInputColor}>Click me!</button>
-//     </div>
-//   )
-// }
-
-const Component = React.memo((props: any) => {
-  console.log('RENDER')
-  return (
-    <div>
-      Component
-      <button onClick={props.setMergedState}>Set Merged State</button>
-    </div>
-  )
-})
-
-// function Component(props: { removeCounter?: () => void }) {
-//   console.log('RENDER')
-//   return (
-//     <div>
-//       Component
-//       <button onClick={props.removeCounter}>Remove from local storage</button>
-//     </div>
-//   )
-// }
-
-// const MemoedComponent = React.memo(Component)
-
-// class Component extends React.PureComponent<{
-//   removeCounter?: () => void
-//   prop?: number
-// }> {
-// shouldComponentUpdate(nextProps: { removeCounter?: () => void; prop?: number }) {
-//   if (this.props.prop !== nextProps.prop) {
-//     return true
-//   }
-
-//   if (this.props.removeCounter !== nextProps.removeCounter) {
-//     return true
-//   }
-
-//   return false
-// }
-
-//   render() {
-//     console.log('RENDER')
-//     return (
-//       <div>
-//         Component
-//         <button onClick={this.props.removeCounter}>Remove from local storage</button>
-//       </div>
-//     )
-//   }
-// }
+// feed -> scroll -> toast()
+// login -> web socket -> comes notification -> toast()
+// create repository -> fetch() -> toast()
+// logout -> click -> toast()
+// regiseter -> submit -> toast()
 
 export default App
--- src/toast/Toast.tsx
+++ src/toast/Toast.tsx
@@ -1,34 +1,56 @@
 import React, { useContext, createContext } from 'react'
-type ToastType = 'success' | 'error' | 'info' | 'warning' | 'debug'
-type ToastPosition = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+// type ToastType = 'success' | 'error' | 'info' | 'warning' | 'debug'
+// type ToastPosition = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
 
-export type ToastProps = {
-  type: ToastType
+// export type ToastProps = {
+//   type: ToastType
+//   message: string
+//   position?: ToastPosition
+//   duration?: number
+// }
+
+// const typeToColor: Record<ToastType, string> = {
+//   success: 'green',
+//   error: 'red',
+//   info: 'blue',
+//   warning: 'yellow',
+//   debug: 'white',
+// }
+
+type ToastType = {
   message: string
-  position?: ToastPosition
-  duration?: number
+  color: string
 }
 
-const typeToColor: Record<ToastType, string> = {
-  success: 'green',
-  error: 'red',
-  info: 'blue',
-  warning: 'yellow',
-  debug: 'white',
-}
+const ToastContext = createContext<((toast: ToastType) => void) | undefined>(undefined)
 
-// export const ToastContext = createContext<((props: ToastProps) => void) | undefined>(
-//   undefined
-// )
+export function ToastProvider({ children }: { children: React.ReactNode }) {
+  const [toasts, setToasts] = React.useState<ToastType[]>([])
 
-function Toast(props: ToastProps) {
+  const createToast = React.useCallback((toast: ToastType) => {
+    setToasts(prevToasts => prevToasts.concat(toast))
+  }, [])
+
   return (
-    <div>
-      <div style={{ backgroundColor: typeToColor[props.type] }}>{props.message}</div>
-    </div>
+    <ToastContext.Provider value={createToast}>
+      {children}
+      {toasts.map((toast, i) => (
+        <div key={i} style={{ backgroundColor: toast.color }}>
+          {toast.message}
+        </div>
+      ))}
+    </ToastContext.Provider>
   )
 }
 
-// toast({ type: 'success', message: 'You have logged in' })
-// const toast = useContext(ToastContext)
-// const toast = useToast()
+export function useToast() {
+  const toast = useContext(ToastContext)
+
+  if (toast === undefined) {
+    throw new Error(
+      'createToast is undefined. Make sure you wrap your component in ToastProvider.'
+    )
+  }
+
+  return toast
+}
